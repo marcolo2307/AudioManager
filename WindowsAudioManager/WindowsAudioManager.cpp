bb1d9a9c786ae55fd951a916c1f0ca4a0c998fdf
Marek Przydanek <marcolo2307@gmail.com>
2018-11-08 21:02:46 +0100

    Add Windows manager project

---
+++ WindowsAudioManager/WindowsAudioManager.cpp
@@ -0,0 +1,7 @@
+#define WINDOWS_AUDIO_MANAGER_EXPORTS
+#include "WindowsAudioManager.hpp"
+
+WINDOWS_AUDIO_MANAGER_API IAudioManager* CreateAudioManager()
+{
+	return nullptr;
+}
---
+++ WindowsAudioManager/WindowsAudioManager.hpp
@@ -0,0 +1,26 @@
+#ifndef WINDOWS_AUDIO_MANAGER_HPP
+#define WINDOWS_AUDIO_MANAGER_HPP
+
+#include "../AudioManager/AudioManager.hpp"
+
+#ifndef __WIN32
+#	ifdef WINDOWS_AUDIO_MANAGER_EXPORTS
+#		define WINDOWS_AUDIO_MANAGER_API __declspec(dllexport)
+#	else
+#		define WINDOWS_AUDIO_MANAGER_API __declspec(dllimport)
+#	endif
+#else
+#	define WINDOWS_AUDIO_MANAGER_API
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+WINDOWS_AUDIO_MANAGER_API IAudioManager* CreateAudioManager();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WINDOWS_AUDIO_MANAGER_HPP
